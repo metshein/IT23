aa27334d9af853e7b886e3e8451fce9c100aeac8
metshein <metshein@gmail.com>
2024-01-10 13:22:33 +0200

    Create is.too3 r1.py

---
+++ is.too3 r1.py
@@ -0,0 +1,27 @@
+#TÜ iseseisev töö
+#M.Metshein 
+#18.12.23, 10.01.24
+
+import random
+
+
+
+
+#sissetulekud
+
+
+
+
+#rebased
+
+# fail = open("rebased.txt", encoding="utf-8")
+# vastuvoetud = []
+# for rida in fail:
+#     vastuvoetud.append(int(rida))
+
+# #2011-2019
+# #print(vastuvoetud)
+# aasta = 2011
+# print(f"Aastal {aasta} võeti vastu {vastuvoetud[aasta-2011]} õpilast")
+
+# fail.close()
