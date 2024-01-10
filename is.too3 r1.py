ab23c3ce5a27a772490fe149e011228d088d92ea
metshein <metshein@gmail.com>
2024-01-10 13:59:04 +0200

    a

--- is.too3 r1.py
+++ is.too3 r1.py
@@ -6,10 +6,21 @@
 siiamidagi = input("Anna failinimi: ")
 fail = open(siiamidagi, encoding="utf-8")
 print("Muusikapalade valik:")
+nr = 1
+for i in fail:
+    print(f"{nr}. {i}",end="")
+    nr+=1
+valik = int(input("\n Vali laulu number: "))
+
+fail.seek(0) #keri faili algusesse
+nr = 1
+for i in fail:
+    if nr == valik:
+        print(i)
+    nr+=1
 
 
-
-
+input()
 #sissetulekud
 fail = open("konto.txt")
 for i in fail:
