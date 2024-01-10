fc757593b1e91bd07da660472cd34743e28e98f4
metshein <metshein@gmail.com>
2024-01-10 14:41:29 +0200

    Update is.too3 r1.py

--- is.too3 r1.py
+++ is.too3 r1.py
@@ -2,6 +2,22 @@
 #M.Metshein 
 #18.12.23, 10.01.24
 
+import datetime
+paev = datetime.datetime.now().day
+
+#tahvli juurde
+fail = open("nimekiri.txt", encoding="utf-8")
+nr = 1
+for nimi in fail:
+    if nr == paev:
+        print(nimi, end="")
+    nr+=1
+
+
+
+
+
+input()
 #jukebox
 siiamidagi = input("Anna failinimi: ")
 fail = open(siiamidagi, encoding="utf-8")
