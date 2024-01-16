9c82e18b9551929be270bc311636d7bda772c308
metshein <metshein@gmail.com>
2024-01-16 10:35:31 +0200

    d

---
+++ grupp2_isyl.py
@@ -0,0 +1,16 @@
+#Mario
+#16.01.21
+
+
+
+
+# 2. Vanused
+# 	loo vanuste loend 1p
+# 	leia numbrite suurim ja väikseim arv  1p
+# 	kogusumma  1p
+# 	keskmine 1p
+
+vanused=[7,6,5,19,18,18,17,20,15,11,7,10,13,12,20,11,10,14,18,14,24,6,17,16,6,17,5,13,11]
+
+def vanuste_stat(loend):
+    pass
